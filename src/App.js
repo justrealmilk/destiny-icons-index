fc48edea59f2cae28637dc8c0fb1b6faab7d0563
Tom Chapman <chapman@live.com.au>
2019-11-10 17:06:35 +1000

    .

--- src/App.js
+++ src/App.js
@@ -2,96 +2,46 @@ import React from 'react';
 
 import './App.css';
 
+import data from './data.json';
+
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      data: false
-    };
+    this.state = {};
   }
 
-  async componentDidMount() {
-    const root = await fetch(`https://api.github.com/repos/justrealmilk/destiny-icons/contents/`, {
-      headers: {
-        Authorization: 'token dd9c981789b0763fcc960f5b6f93cec10005a14a'
-      }
-    })
-      .then(response => {
-        return response.json();
-      })
-      .catch(e => {
-        console.log(e);
-      });
-
-    await Promise.all(root.filter(i => i.type === 'dir').map(async d => {
-      const files = await fetch(d.url, {
-        headers: {
-          Authorization: 'token dd9c981789b0763fcc960f5b6f93cec10005a14a'
-        }
-      })
-        .then(response => {
-          return response.json();
-        })
-        .catch(e => {
-          console.log(e);
-        });
-
-      if (files) {
-        await Promise.all(files.map(async f => {
-          const file = await fetch(f.download_url)
-            .then(response => {
-              return response.text();
-            })
-            .catch(e => {
-              console.log(e);
-            });
-
-            f.trueContent = file;
-        }));
-
-        d.files = files;
-      }
-    }));
-
-    console.log(root.filter(i => i.type === 'dir'))
-
-    this.setState({
-      data: root.filter(i => i.type === 'dir')
-    });
+  componentDidMount() {
+    console.log(data)
   }
 
   render() {
-    if (!this.state.data) {
-      return (
-        <div className='view' id='loading'>
-          <p>one sec</p>
-        </div>
-      );
-    } else {
-      return (
-        <div className='view' id='index'>
-          {this.state.data.map((d, i) => {
-            return (
-              <div key={i} className='dir'>
-                <h2>{d.name}</h2>
-                <div className='files'>
-                  {d.files.map((f, i) => {
 
-                    return (
-                      <div key={i} className='file'>
-                        <img src={`data:image/svg+xml;base64,${window.btoa(f.trueContent)}`} alt={f.name} />
-                        <div className='name'>{f.name}</div>
-                      </div>
-                    )
-                  })}
-                </div>
+
+
+    return (
+      <div className='view' id='index'>
+        {data.children.map((f, i) => {
+          return (
+            <div key={i} className='dir'>
+              <h2>{f.name}</h2>
+              <div className='files'>
+                {f.children.filter(c => c.type === 'file').map((f, i) => {
+
+                  return (
+                    <div key={i} className='file'>
+                      <img src={`data:image/svg+xml;base64,${window.btoa(f.content)}`} alt={f.name} />
+                      <div className='name'>{f.name}</div>
+                    </div>
+                  )
+                })}
               </div>
-            )
-          })}
-        </div>
-      );
-    }
+            </div>
+          )
+        })}
+      </div>
+    );
+    
   }
 }
 
